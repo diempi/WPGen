af50b23abb76828fd4484a299a453a7d397cf3b8
diempi <dev@diempi>
2013-10-21 10:41:17 +0200

    mise à jour de la page home.php

--- home.php
+++ home.php
@@ -1,6 +1,23 @@
 <?php get_header( ); ?>
+    <section id="showcase">
+        <h1 class="bigtitle"><?php the_title(); ?></h1>
+        <div id="branding">
+            <?php 
+                $techs = get_terms( 'techniques');
+            ;?>
+            <ul>
+            <?php
+                    foreach ($techs as $tech) {
+            ;?>
+                <li class="techniques">
+                        <a href="<?php echo get_term_link($tech); ?>" title="<?php echo ($tech->name); ?>" data-hover="<?php echo ($tech->name); ?>"><span class="fontel icon-<?php echo ($tech->name); ?>"></span><span class="icon-text"><?php echo ($tech->name); ?></span></a>
+                </li>
+             <?php       
+                }      
+            ; ?>
+        </div>
+    </section>
             <section id="content">
-                <h1>Mes projets récents</h1>
                 <ul class="galerie">
                     <?php 
                         $arg = array('post_type' => 'works');
@@ -8,24 +25,18 @@
                     ;?>
                     <?php if(have_posts()): ?>
                         <?php while($loop->have_posts()): $loop->the_post();?>
-                            <li class="recent-works" itemscope itemtype="http://schema.org/Blog">
-                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" itemprop="url"><?php the_post_thumbnail(); ?>
-                                    <h2 class="title" itemprop="name"><?php the_title(); ?></h2>
-                                    <p itemprop="keywords"><?php the_terms($post->ID,'annees','Années: ',' - ',' '); ?></p>
+                            <li class="recent-works"  itemscope itemtype="http://schema.org/Article">
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" itemprop="url">
+                                    <div class="thumb" itemprop="image"><?php the_post_thumbnail(); ?></div> 
+                                    <h3 class="title" itemprop="name"><?php the_title(); ?></h3>
                                     <p><?php the_terms($post->ID,'techniques','Crée avec ',' - ',' '); ?></p>
+                                    <p itemprop="description"><?php the_terms($post->ID,'annees','Années: ',' - ',' '); ?></p>
                                 </a>
                             </li>
                         <?php endwhile; ?>
                     <?php endif; ?>
                 </ul>
-                <h1>Je parle</h1>
-                <ul>
-                    <li class="logo"><img src="<?php bloginfo( 'template_directory' ); ?>/img/html5.png" alt="Logo de HTML5" title="Logo de HTML5"></li>
-                    <li class="logo"><img src="<?php bloginfo( 'template_directory' ); ?>/img/css3.png" alt="Logo de CSS23" title="Logo de CSS3"></li>
-                    <li class="logo"><img src="<?php bloginfo( 'template_directory' ); ?>/img/php.png" alt="Logo de PHP" title="Logo de PHP"></li>
-                    <li class="logo"><img src="<?php bloginfo( 'template_directory' ); ?>/img/wp.png" alt="Logo de Wordress" title="Logo de Wordpress"></li>
-                    <li class="logo"><img src="<?php bloginfo( 'template_directory' ); ?>/img/drupal.png" alt="Logo de Drupal" title="Logo de Drupal"></li>
-                </ul>
+
                 <div id="main-footer"></div>
             </section><!-- END MAIN -->
         </div>
