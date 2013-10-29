8378803e971e5e12eadf8e0ae8ae908ee33b9200
diempi <dev@diempi>
2013-10-29 16:03:38 +0100

    Mise à jour du lien de contactez-moi sur la page à propos

--- page-about.php
+++ page-about.php
@@ -12,7 +12,7 @@
                   <figcaption>Didier Gabiam</figcaption>
                 </figure>
                 <section id="me">
-                    Un mercenaire ça vous parle? Oui vous savez ces gens qu'on paie pour faire un travail... He bén j'en fais partie! Il n'est pas question d'éliminer qui que ce soit (même si je pense qu'Internet Explorer mérite un sort...bref),il s'agit plutôt de louer mes services pour aboutir à VOTRE objectif! Alors qu'attendez-vous? <a href="contact.html" title="cliquez vite et contactez-moi">Contactez-moi</a> pour vos projets dès maintenant.
+                    Un mercenaire ça vous parle? Oui vous savez ces gens qu'on paie pour faire un travail... He bén j'en fais partie! Il n'est pas question d'éliminer qui que ce soit (même si je pense qu'Internet Explorer mérite un sort...bref),il s'agit plutôt de louer mes services pour aboutir à VOTRE objectif! Alors qu'attendez-vous? <a href="contact" title="cliquez vite et contactez-moi">Contactez-moi</a> pour vos projets dès maintenant.
                 </section>
                 <div id="main-footer"></div>
             </section><!-- END MAIN -->
